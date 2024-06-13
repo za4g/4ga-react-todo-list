fd0e0256399f22fcee16899948359cc8bf3fbdce
abuameen4g <shahedkhan1970@yahoo.com>
2024-06-13 08:59:10 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
-import "./App.css";
-import Container from "./Container.js";
-import Header from "./Header.js";
-import Hero from "./Hero.js";
-import Cards from "./Cards.js";
-import Footer from "./Footer.js";
+import { useEffect, useState } from "react";
 
 function App() {
+  return <Todos />;
+}
+
+function Todos() {
+  const [itemCount, setItemCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      if (inputValue.trim() !== "") {
+        setTodos([...todos, inputValue]);
+        setInputValue("");
+        setItemCount(itemCount + 1);
+      }
+    }
+  };
+
+  useEffect(() => {}, [itemCount, todos]);
+
   return (
-    <Container>
-      <Header />
-      <Hero />
-      <Cards />
-      <Footer />
-    </Container>
+    <div className="todo-component">
+      <div>
+        <h1 className="todo-header">todos</h1>
+      </div>
+      <div className="todo-container">
+        <div>
+          <input
+            className="todo-input"
+            type="text"
+            placeholder="What needs to be done?"
+            onKeyDown={handleKeyDown}
+            onChange={(e) => setInputValue(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <ul className="todo-list">
+            {todos.map((todo, index) => (
+              <div key={index} className="todo-item">
+                <li>{todo}</li>
+              </div>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <p className="item-count">{itemCount} items left</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
